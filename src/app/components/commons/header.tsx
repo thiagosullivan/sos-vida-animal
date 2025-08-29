8e622100556f0f0e1d77313b563ea3ce91b72cd8
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-08-29 18:59:25 -0300

    feat: add header component

---
+++ src/app/components/commons/header.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  FaEnvelope,
+  FaFacebookSquare,
+  FaInstagramSquare,
+} from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="py-7 border-black border-b-1 ">
+      <div className="w-full mx-auto max-w-7xl flex justify-between items-center text-gray-primary font-readex">
+        <Link href="/">
+          <Image src="/sos-logo.png" width={67} height={70} alt="SOS logo" />
+        </Link>
+        <nav>
+          <ul className="flex items-center gap-7 ">
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                O que fazemos
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Informações
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Final Feliz
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Ajude a ajudar
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Contato
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <ul className="flex items-center gap-5">
+          <li>
+            <Link href="https://facebook.com">
+              <FaFacebookSquare size={30} />
+            </Link>
+          </li>
+          <li>
+            <Link href="https://instagram.com">
+              <FaInstagramSquare size={30} />
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <FaEnvelope size={30} />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
