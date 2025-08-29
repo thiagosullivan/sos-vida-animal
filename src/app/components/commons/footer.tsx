a3d57bc47e49833c39f82cf4f1715756f746a8ca
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-08-29 20:40:47 -0300

    feat: add footer component

---
+++ src/app/components/commons/footer.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  FaEnvelope,
+  FaFacebookSquare,
+  FaInstagramSquare,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="border-black border-t-1 mt-28">
+      <div className="w-full mx-auto max-w-7xl flex justify-between items-center text-gray-primary font-readex py-7">
+        <Link href="/">
+          <Image src="/sos-logo.png" width={67} height={70} alt="SOS logo" />
+        </Link>
+        <nav>
+          <ul className="flex items-center gap-7 uppercase">
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                O que fazemos
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Informações
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Final Feliz
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Ajude a ajudar
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="hover:text-orange-secondary">
+                Contato
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <ul className="flex items-center gap-5">
+          <li>
+            <Link
+              className="hover:text-orange-secondary"
+              href="https://facebook.com"
+            >
+              <FaFacebookSquare size={30} />
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-orange-secondary"
+              href="https://instagram.com"
+            >
+              <FaInstagramSquare size={30} />
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:text-orange-secondary" href="/">
+              <FaEnvelope size={30} />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="text-center bg-orange-secondary py-2">
+        <p className="font-light font-readex text-xs">
+          Desenvolvido por{" "}
+          <Link href="/" className="font-bold uppercase">
+            ATS Web Solution
+          </Link>{" "}
+          | Copyright &copy; 2025. Todos os direitos reservados.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
