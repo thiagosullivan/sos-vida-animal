2b29e243efe57d0a503c60ca1383e0e8e4c6ce3f
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-01 01:19:14 -0300

    feat:  add mobile styling for donate page

--- src/app/doacao/page.tsx
+++ src/app/doacao/page.tsx
@@ -4,8 +4,8 @@ import { RiBankLine } from "react-icons/ri";
 
 const DonatePage = () => {
   return (
-    <div className="w-full mx-auto max-w-7xl py-10 px-4 font-readex flex justify-around items-center h-[calc(100vh-276px)]">
-      <div className="max-w-[500px] w-full">
+    <div className="w-full mx-auto max-w-7xl py-10 px-4 font-readex flex flex-col lg:flex-row justify-center lg:justify-around items-center lg:h-[calc(100vh-276px)]">
+      <div className="lg:block hidden max-w-[500px] w-full">
         <Image
           src="/dog-3.png"
           alt="Cachorro"
@@ -15,19 +15,19 @@ const DonatePage = () => {
           className="h-auto w-full"
         />
       </div>
-      <div className="flex flex-col items-end gap-10 max-w-[500px]">
-        <h1 className="text-5xl text-orange-secondary font-fredoka">
+      <div className="flex flex-col items-center lg:items-end gap-10 max-w-[500px] mb-10">
+        <h1 className="text-center lg:text-end text-5xl text-orange-secondary font-fredoka">
           Faça uma doação
         </h1>
         <div className="w-[200px] h-1.5 bg-gray-primary"></div>
-        <p className="font-light text-end">
+        <p className="font-light text-center lg:text-end">
           Ajude-nos com uma contribuição. Todos os valores são bem-vindos.{" "}
           <span className="underline">
             A sua doação faz a diferença para a continuidade do projeto!
           </span>
         </p>
-        <div className="flex justify-between w-full gap-3">
-          <div className="flex flex-col items-start border-l-1 pl-4">
+        <div className="flex flex-col items-start lg:items-start lg:flex-row lg:justify-between w-full gap-3">
+          <div className="flex flex-col items-start lg:border-l-1 pb-4 lg:pl-4">
             <div className="flex items-center gap-2">
               <RiBankLine size={30} className="fill-orange-secondary" />
               <p>Transferência bancária:</p>
@@ -44,7 +44,7 @@ const DonatePage = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-start border-l-1 pl-4">
+          <div className="flex flex-col items-start lg:border-l-1 lg:pl-4">
             <div className="flex items-center gap-2">
               <FaPix size={30} className="fill-orange-secondary" />
               <p>Pix:</p>
@@ -56,6 +56,16 @@ const DonatePage = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="lg:hidden block max-w-[165px] w-full">
+        <Image
+          src="/dog-3.png"
+          alt="Cachorro"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
       </div>
     </div>
   );
